102a5505393f35693052e894a9b6a4298702f267
wt12318 <1650464505@qq.com>
2022-03-24 14:30:02 +0800

    update txt2binding function

---
+++ R/txt2binding.R
@@ -0,0 +1,138 @@
+#' Convert TXT file to mutated peptides
+#'
+#' @description This function extract neo-peptides produced from the mutation recorded in the TXT file using the scripts provided by annovar.The txt file is actually
+#' a annovar input file, and the first five space- or tab- delimited columns represent chromosome, start position, end position, the reference nucleotides and the observed nucleotides
+#' @param annovar_path Character, the install path of annovar.
+#' @param txt_path Character, the path of TXT file needed to be processed.
+#' @param genome_version Character, which genome build version of the TXT file, can be hg19 or hg38
+#'
+#' @details The command run is like: annotate_variation.pl -out out_dir -build hg19 anaovar_input.txt humandb/ -hgvs -dbtype ensGene
+#'          coding_change.pl common_driver.exonic_variant_function humandb/hg19_ensGene.txt humandb/hg19_ensGeneMrna.fa -includesnp -onlyAltering --outfile coding.txt
+
+#' @return A dataframe containg wildtype and the mutated protein sequence.
+#' @export
+#'
+#' @examples
+#' pep <- txt2pep(annovar_path = annovar_path,txt_path = system.file("extdata", "test_avinput.txt", package = "MHCbinding"),
+#'                genome_version = "hg19")
+txt2pep <- function(annovar_path,txt_path,
+                    genome_version=c("hg19","hg38")){
+  genome_version <- match.arg(genome_version)
+  mut <- data.table::fread(txt_path,data.table = F)
+  mut <- mut[,1:5]
+  colnames(mut) <- c("chr","start","end","ref","alt")
+
+  temp_dir <- tempdir()
+  comm_annotate <- paste0(annovar_path,"/annotate_variation.pl -out ",temp_dir,"/test -build ",genome_version," ",txt_path,
+                          " ",annovar_path,"/humandb/ -hgvs -dbtype ensGene")
+  system(comm_annotate)
+  comm_coding <- paste0(annovar_path,"/coding_change.pl ",temp_dir,"/test.exonic_variant_function ",annovar_path,"/humandb/",
+                        genome_version,"_ensGene.txt ",annovar_path,"/humandb/",genome_version,"_ensGeneMrna.fa ",
+                        "-includesnp -onlyAltering --outfile ",temp_dir,"/coding.txt")
+  system(comm_coding)
+
+  dt <- seqinr::read.fasta(file = paste0(temp_dir,"/coding.txt"),seqtype = "AA",as.string = TRUE,whole.header = T)
+
+  variants <- names(dt)[seq(2,length(dt),by=2)]
+  pos_alter <- stringr::str_extract_all(variants,"p[.].+ p") %>% gsub(" p","",.)
+  lines <- stringr::str_extract_all(variants,"line.+ ENST") %>% gsub(" ENST","",.) %>%
+    gsub("line","",.) %>% as.numeric()
+
+  mut_need <- mut[lines,]
+  pep_seq_mt <- dt[seq(2,length(dt),by=2)] %>% as.character()
+  pep_seq_wt <- dt[seq(1,length(dt),by=2)] %>% as.character()
+  mut_need$seq_wt <- pep_seq_wt
+  mut_need$seq_mt <- pep_seq_mt
+  mut_need$pos_alter <- pos_alter
+  files_exist <- c(list.files(temp_dir,pattern = "test",full.names = T),
+                   list.files(temp_dir,pattern = "coding.txt",full.names = T))##remove all temp files
+  file.remove(files_exist)
+  return(mut_need)
+}
+
+#' Convert TXT file to mutated peptides and extract proper range of amino acids
+#'
+#' @description This function use \code{\link{txt2pep}} to convert TXT file to mutated peptides, and use \code{\link{extractSeq}} to extract proper range of amino acids.
+#' @param annovar_path Character, the install path of annovar.
+#' @param txt_path Character, the path of TXT file needed to be converted.
+#' @param genome_version Character, which genome build version of the TXT file, can be hg19 or hg38
+#' @param len Numeric, the epitope length needed to predicted.
+#'
+#' @return A dataframe containg wildtype and the mutated protein sequence, plus exacted sequence from mutated peptides.
+#' @export
+#'
+#' @examples
+#' txt2seq(annovar_path = annovar_path,txt_path = system.file("extdata", "test_avinput.txt", package = "MHCbinding"),
+#'         genome_version = "hg19",len = 9)
+txt2seq <- function(annovar_path,txt_path,
+                    genome_version=c("hg19","hg38"),len){
+
+  pep <- txt2pep(annovar_path = annovar_path,txt_path = txt_path,
+                 genome_version = genome_version)
+
+  pep <- pep %>%
+    mutate(pos = stringr::str_extract(pos_alter,"[0-9]+") %>% as.numeric()) %>%
+    rowwise() %>%
+    mutate(indel=ifelse(ref != "-" & alt != "-" & nchar(ref) == 1 & nchar(alt) == 1,"FALSE","TRUE")) %>%
+    as.data.frame()
+  ext_seqs_mt <- mapply(extractSeq,seq=pep$seq_mt,pos=as.numeric(pep$pos),len=as.numeric(len),indel=pep$indel) %>% unname()
+  #ext_seqs_wt <- mapply(extractSeq,seq=pep$seq_wt,pos=pep$pos,len=len,indel=pep$indel) %>% unname()
+  ##remove the last star
+  a <-  substr(ext_seqs_mt,nchar(ext_seqs_mt),nchar(ext_seqs_mt))=="*"
+  #b <-  substr(ext_seqs_wt,nchar(ext_seqs_wt),nchar(ext_seqs_wt))=="*"
+  ext_seqs_mt[a] <- gsub("\\*","",ext_seqs_mt[a])
+  #ext_seqs_wt[b] <- gsub("\\*","",ext_seqs_wt[b])
+
+  #pep$ext_seqs_wt <- ext_seqs_wt
+  pep$ext_seqs_mt <- ext_seqs_mt
+  return(pep)
+}
+
+#' Predict peptide MHC binding based on TXT file.
+#'
+#' @description This function use \code{\link{txt2seq}} to exacted mutated "new peptide" from TXT file, and predicted the binding affinity of these peptide with specific MHC allele using \code{\link{general_mhcbinding}}
+#' @param annovar_path Character, the install path of annovar.
+#' @param txt_path Character, the path of TXT file needed to be converted.
+#' @param genome_version Character, which genome build version of the VCF file, can be hg19 or hg38
+#' @param mhc_type MHC class, can be MHC-I or MHC-II.
+#' @param pep_length A numeric or character vector, indicating the length for which to make predictions. For MHC-I, the length can be 8-15, for MHC-II, the length can be 11-30 or asis (take the length of input sequence as the peptide length)
+#' @param allele A character vector of HLA alleles, available alleles for specific method can be obtained by \code{\link{available_alleles}}
+#' @param pre_method Character, indicating the prediction method. Available methods for MHC-I or MHC-II can be obtained by \code{\link{available_methods}}
+#'
+#' @return A dataframe contains the predicted IC50 and precentile rank (if available).
+#' @export
+#'
+#' @examples
+#' test <- txt2binding(annovar_path = "~/software/annovar/",txt_path = system.file("extdata", "test_avinput.txt", package = "MHCbinding"),
+#'                     genome_version = "hg19",mhc_type = "MHC-I",pep_length = c(9,10),
+#'                     allele = c("HLA-A*01:01", "HLA-A*03:01"),pre_method = "ann")
+txt2binding <- function(annovar_path,txt_path,
+                        genome_version=c("hg19","hg38"),mhc_type,pep_length,allele,
+                        pre_method){
+  res <- vector("list",length = length(pep_length))
+  names(res) <- pep_length
+  for (i in seq_along(res)){
+    res[[i]] <- txt2seq(annovar_path = annovar_path,txt_path = txt_path,genome_version = genome_version,
+                        len = pep_length[i])
+  }
+  res <- dplyr::bind_rows(res,.id = "predicted_length")
+
+  pre_res <- vector("list",length = length(pep_length))
+  names(pre_res) <- pep_length
+  for (i in seq_along(pre_res)){
+    pep <- res[res$predicted_length == names(pre_res)[i],"ext_seqs_mt"]
+    pre_res[[i]] <- MHCbinding:::general_mhcbinding(mhc_type = mhc_type, length = pep_length[i],
+                                                    allele = allele,pre_method = pre_method, peptide = pep)
+  }
+
+  pre_res <- dplyr::bind_rows(pre_res)
+  res <- res %>% mutate(index = paste(predicted_length,map(table(res$predicted_length) %>% unname,~seq(1,.x)) %>% unlist(),sep = ":"))
+  pre_res <- pre_res %>% mutate(index=paste(length,seq_num,sep = ":"))
+  pre_res <- left_join(
+    pre_res %>% rename(pep_start=start,pep_end=end),
+    res %>% select(chr,start,end,ref,alt,index)
+  ) %>% select(-index)
+
+  return(pre_res)
+}
+
