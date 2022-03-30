11b11b60e27f3e4d6d8b53066a66051c3cdf27de
wt12318 <1650464505@qq.com>
2022-03-30 10:03:59 +0800

    fix method alleles bug

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -13,38 +13,30 @@ usethis::use_data(mhcIIbinding_api_methods,overwrite = T)
 ### MHC-I allele
 for (i in mhcIbinding_api_methods$method){
   i <- gsub(" \\(netmhcpan\\)","",i)
+  if (i == "recommended"){i <- "IEDB_recommended"}
   file.create(paste0("~/tmp/mhciallele_",i))
-  comm <- paste0('curl --data "method=',i,'&species=human" http://tools-cluster-interface.iedb.org/tools_api/mhci/ > ',paste0("~/tmp/mhciallele_",i))
+  comm <- paste0("~/software/mhc_i/src/predict_binding.py ",i,paste0(" mhc > ~/tmp/mhciallele_",i))
   mess <- try(system(comm))
-  if (mess == 0){
-    message("Succeed !")
-  }else{
-
-    for (j in 1:10){
-      warning(paste0("Failed retrieving, retrying ",j," times"))
-      mess1 <- try(system(comm))
-      if (mess1 == 0){
-        message("Succeed !")
-        break
-      }
-    }
-    if (j == 10){
-      stop("Failed retrieving, stop", immediate. = TRUE)
-    }
-  }
 }
-files <- list.files("~/tmp",full.names = T)
+files <- list.files("~/tmp",full.names = T,pattern = "mhciallele_")
 res <- purrr::map(files,
                   function(x){
                     dt <- data.table::fread(x,blank.lines.skip = T,data.table = F) %>% as_tibble()
                     dt <- dt[1:(nrow(dt)-2),]
-                    dt$method <- gsub("/home/wt/tmp/","",x)
+                    dt$method <- gsub("/home/data/t040201/tmp/","",x)
                     return(dt)})
 res <- bind_rows(res)
 colnames(res)[1] <- "alleles"
 res$method <- gsub("mhciallele_","",res$method)
 mhcIallele <- res
-usethis::use_data(mhcIallele)
+mhcIallele <- mhcIallele %>%
+  filter(alleles == "human") %>%
+  distinct(MHC,method,.keep_all = T) %>%
+  select(-PeptideLength)
+mhcIallele <- mhcIallele %>%
+  select(-alleles) %>%
+  rename(alleles=MHC)
+usethis::use_data(mhcIallele,overwrite = T)
 
 ###MHC-II
 for (i in mhcIIbinding_api_methods$method){
