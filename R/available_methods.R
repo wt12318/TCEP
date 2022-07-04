40cf36e22e1fb496432ca021d7263a001aeb5e1f
wutao <1650464505@qq.com>
2022-07-04 11:29:14 -0400

    add other method

--- R/available_methods.R
+++ R/available_methods.R
@@ -7,29 +7,33 @@
 #' @export
 #'
 #' @examples
-#' available_methods("api","MHC-I")
-available_methods <- function(get_methods=c("api","client"),
-                              pre_type=c("MHC-I","MHC-II")){
-  get_methods <- match.arg(get_methods)
+#' available_methods("MHC-I","Binding")
+available_methods <- function(pre_type=c("Binding","Processing","Immuno")){
+
   pre_type <- match.arg(pre_type)
-  if (get_methods == "api"){
-    if(pre_type == "MHC-I"){
-      print(dplyr::as_tibble(MHCbinding::mhcIbinding_api_methods))
-      return(MHCbinding::mhcIbinding_api_methods[,1])
-    }else{
-      print(dplyr::as_tibble(MHCbinding::mhcIIbinding_api_methods))
-      return(MHCbinding::mhcIIbinding_api_methods[,1])
-    }
-  }else{
-    if(pre_type == "MHC-I"){
-      dt <- MHCbinding::mhcIbinding_api_methods
-      dt$method[9] <- "IEDB_recommended"
-      print(dplyr::as_tibble(dt))
-      return(dt[,1])
-    }else{
-      print(dplyr::as_tibble(MHCbinding::mhcIIbinding_client_methods))
-      return(c("comblib", "consensus3", "IEDB_recommended", "netmhciipan_el",
-               "netmhciipan_ba","nn_align", "smm_align","sturniolo"))
-    }
+  if (pre_type == "Binding"){
+    dt_mhci <- MHCbinding::mhcIbinding_api_methods
+    dt_mhci$method[9] <- "IEDB_recommended"
+    cat(crayon::green("HLA-I antige binding methods: ","\n"))
+    print(dplyr::as_tibble(dt_mhci))
+    methods_mhci <- c(dt_mhci[,1],"mhcflurry","mhcnuggets")
+
+    dt_mhcii <- data.frame(
+      method = c("comblib", "consensus3", "IEDB_recommended", "netmhciipan_el",
+                 "netmhciipan_ba","nn_align", "smm_align","sturniolo"),
+      version = c("1.0","2.22","2.22","4.0","4.0","2.3","1.1","1.0")
+    )
+    cat(crayon::green("HLA-II antige binding methods: ","\n"))
+    print(dplyr::as_tibble(dt_mhcii))
+    methods_pre <- c(methods_mhci,dt_mhcii$method)
+  }else if (pre_type == "Processing"){
+    methods_pre <- c("Netchop","NetCTLpan")
+    cat(crayon::green("Antigen processing methods: ","\n"))
+    print(methods_pre)
+  }else {
+    methods_pre <- c("IEDB","NetTepi","PRIME","iPred","DeepImmuno","Seq2Neo-CNN")
+    cat(crayon::green("Antigen immunogenicity methods: ","\n"))
+    print(methods_pre)
   }
+  return(invisible(methods_pre))
 }
